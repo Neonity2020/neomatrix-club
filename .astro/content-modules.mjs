7dc2218609873373f2325b179d8b361d6a6f530d
mriwiki <71009631+Neonity2020@users.noreply.github.com>
2026-03-22 15:11:31 +0800

    chore: remove and ignore .astro cache folder

--- .astro/content-modules.mjs
+++
@@ -1,7 +0,0 @@
-
-export default new Map([
-["src/content/blog/ai-tools-workflow-2025.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fai-tools-workflow-2025.mdx&astroContentModuleFlag=true")],
-["src/content/blog/lifelong-learning-system.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Flifelong-learning-system.mdx&astroContentModuleFlag=true")],
-["src/content/blog/one-person-company-playbook.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fone-person-company-playbook.mdx&astroContentModuleFlag=true")],
-["src/content/blog/super-individual-age.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fsuper-individual-age.mdx&astroContentModuleFlag=true")]]);
-		
